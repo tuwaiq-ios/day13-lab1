17e7ef10de691f5ab92dafd96628228f69d0ca8e
Jawaher <jawaher.hass@gmail.com>
2021-10-12 23:25:25 +0300

    Done

---
+++ Day13/Day13/VC2.swift
@@ -0,0 +1,22 @@
+//
+//  VC2.swift
+//  Day13
+//
+//  Created by MacBook on 06/03/1443 AH.
+//
+
+import UIKit
+
+class VC2:  UIViewController {
+    
+    
+    var L2 : String?
+    @IBOutlet weak var label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label1.text = L2
+    }
+ 
+    
+}
