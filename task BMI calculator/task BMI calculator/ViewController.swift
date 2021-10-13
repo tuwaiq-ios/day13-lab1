db9c279b57c3291685028b54de6777f621b011c0
MacBook <macbook@MacBook-Air-fatima.local>
2021-10-13 09:23:47 +0300

    taskes 13

---
+++ task BMI calculator/task BMI calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task BMI calculator
+//
+//  Created by MacBook on 07/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
