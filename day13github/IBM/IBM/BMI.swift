82184ee67ab19c73894aeae38a45861a35eacb9c
Kholod Sultan <kholodsultan@MacBook-Pro-alkhas-b-Kholod.local>
2021-10-14 11:35:29 +0300

    done

---
+++ day13github/IBM/IBM/BMI.swift
@@ -0,0 +1,20 @@
+//
+//  BMI.swift
+//  IBM
+//
+//  Created by Kholod Sultan on 07/03/1443 AH.
+//
+
+import UIKit
+class BMI {
+var height :Double
+var weight:Double
+
+
+init (height:Double, weight:Double){
+    self.height = height
+    self.weight = weight
+}
+
+}
+
